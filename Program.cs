b78a4deea96c1ea0f6616929ebab364640d67516
Kenneth Ringbo <kenneth@ringbo.dk>
2021-04-16 10:58:52 +0200

    Added dotnet project, hello world

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej Specialist!");
+        }
+    }
+}
