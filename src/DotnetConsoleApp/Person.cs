20a23b454be69d17f387f1744fe2450a30c85d1d
Kenneth Ringbo <keringbo@Kenneths-MacBook-Pro.local>
2021-05-07 16:12:31 +0200

    Changed welcome text

--- src/DotnetConsoleApp/Person.cs
+++ src/DotnetConsoleApp/Person.cs
@@ -4,6 +4,6 @@ namespace People
 {
     public class Person : IPerson
     {
-        public string TalkToPerson() => "Hej Specialist!";
+        public string TalkToPerson() => "Hej Specialist! Du er bare for sej :-)";
     }
 }
