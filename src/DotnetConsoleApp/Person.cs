58ddb767347b4d7d0f86e14b40cf54e65e8b5b26
Specialisterne <82635062+specialisterne-training@users.noreply.github.com>
2021-09-28 13:28:31 +0200

    Update Person.cs

--- src/DotnetConsoleApp/Person.cs
+++ src/DotnetConsoleApp/Person.cs
@@ -4,6 +4,6 @@ namespace People
 {
     public class Person : IPerson
     {
-        public string TalkToPerson() => "Hej Specialist! Du er bare for sej :-)";
+        public string TalkToPerson() => "Hej Specialist! Du er bare for sej :-) - og nu med en ny tekst!";
     }
 }
